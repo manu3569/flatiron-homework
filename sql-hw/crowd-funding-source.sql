b79fa2455a115ab0f3a05517d39ed92bf76580e4
manu3569 <manuel.neuhauser@flatironschool.com>
2013-10-03 17:52:59 -0400

    Updated crowd-funding-source.sql to create tables first

--- sql-hw/crowd-funding-source.sql
+++ sql-hw/crowd-funding-source.sql
@@ -1,3 +1,136 @@
+---------------------
+-- BUILD DB SOURCE --
+---------------------
+
+
+-- PROJECTS --
+
+CREATE TABLE projects (
+  id INTEGER PRIMARY KEY,
+  title text,
+  funding_goal INTEGER,
+  start_date text,
+  end_date text
+);
+
+INSERT INTO projects VALUES (NULL, 'iPhone Solar Charger Case', 190000, '2013-10-01', '2013-11-01');
+INSERT INTO projects VALUES (NULL, 'iPhone Wireless Charger', 180000, '2013-10-02', '2013-11-03');
+INSERT INTO projects VALUES (NULL, 'iPhone External Monitor', 170000, '2013-10-03', '2013-11-05');
+INSERT INTO projects VALUES (NULL, 'iPhone USB Taste Sensor', 160000, '2013-10-04', '2013-11-07');
+INSERT INTO projects VALUES (NULL, 'iPhone Invisibility Case', 150000, '2013-10-05', '2013-11-15');
+INSERT INTO projects VALUES (NULL, 'iPhone Screen Projector', 140000, '2013-10-10', '2013-11-11');
+INSERT INTO projects VALUES (NULL, 'iPhone Wool Knit Case', 130000, '2013-10-15', '2013-11-01');
+INSERT INTO projects VALUES (NULL, 'iPhone External SMS Notification LED', 120000, '2013-10-20', '2013-10-30');
+INSERT INTO projects VALUES (NULL, 'iPhone Foldable Keyboard', 110000, '2013-10-30', '2013-11-31');
+INSERT INTO projects VALUES (NULL, 'iPhone Thunderbolt-to-classic Charger Converter', 810000, '2013-12-01', '2013-12-31');
+
+
+-- USERS --
+
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  name text,
+  age INTEGER
+);
+
+INSERT INTO users VALUES (NULL, 'Lavona Parrales', 42);
+INSERT INTO users VALUES (NULL, 'Iliana Yamanaka', 23);
+INSERT INTO users VALUES (NULL, 'Daniela Vida', 54);
+INSERT INTO users VALUES (NULL, 'Cletus Pipes', 62);
+INSERT INTO users VALUES (NULL, 'Ashton Roda', 21);
+INSERT INTO users VALUES (NULL, 'Alexander Handley', 23);
+INSERT INTO users VALUES (NULL, 'Christie Touchet', 54);
+INSERT INTO users VALUES (NULL, 'Sharen Mccubbin', 35);
+INSERT INTO users VALUES (NULL, 'Valerie Heiss', 42);
+INSERT INTO users VALUES (NULL, 'Chasidy Swink', 62);
+INSERT INTO users VALUES (NULL, 'Lovie Byars', 37);
+INSERT INTO users VALUES (NULL, 'Ernestine Ring', 80);
+INSERT INTO users VALUES (NULL, 'Kiley Mcferrin', 15);
+INSERT INTO users VALUES (NULL, 'Hsiu Summerville', 36);
+INSERT INTO users VALUES (NULL, 'Iris Bogle', 42);
+INSERT INTO users VALUES (NULL, 'Meagan Waechter', 44);
+INSERT INTO users VALUES (NULL, 'Karry Sheahan', 32);
+INSERT INTO users VALUES (NULL, 'Michale Minard', 18);
+INSERT INTO users VALUES (NULL, 'Andria Sollars', 26);
+INSERT INTO users VALUES (NULL, 'Kasie Sudler', 28);
+
+
+
+-- PLEDGES --
+
+CREATE TABLE pledges (
+  id INTEGER PRIMARY KEY,
+  amount INTEGER,
+  user_id INTEGER,
+  project_id INTEGER
+);
+
+INSERT INTO pledges VALUES (NULL, 344000, 7, 4);
+INSERT INTO pledges VALUES (NULL, 352100, 12, 10);
+INSERT INTO pledges VALUES (NULL, 266200, 1, 5);
+INSERT INTO pledges VALUES (NULL, 113900, 2, 10);
+INSERT INTO pledges VALUES (NULL, 165800, 12, 9);
+INSERT INTO pledges VALUES (NULL, 191300, 15, 5);
+INSERT INTO pledges VALUES (NULL, 151200, 11, 2);
+INSERT INTO pledges VALUES (NULL, 491900, 1, 7);
+INSERT INTO pledges VALUES (NULL, 21700, 2, 4);
+INSERT INTO pledges VALUES (NULL, 295500, 17, 5);
+INSERT INTO pledges VALUES (NULL, 67500, 8, 9);
+INSERT INTO pledges VALUES (NULL, 27600, 9, 2);
+INSERT INTO pledges VALUES (NULL, 240600, 19, 4);
+INSERT INTO pledges VALUES (NULL, 391900, 2, 4);
+INSERT INTO pledges VALUES (NULL, 17000, 0, 7);
+INSERT INTO pledges VALUES (NULL, 35600, 2, 8);
+INSERT INTO pledges VALUES (NULL, 406400, 20, 2);
+INSERT INTO pledges VALUES (NULL, 411000, 17, 5);
+INSERT INTO pledges VALUES (NULL, 497900, 19, 4);
+INSERT INTO pledges VALUES (NULL, 432500, 15, 10);
+INSERT INTO pledges VALUES (NULL, 85500, 8, 3);
+INSERT INTO pledges VALUES (NULL, 97200, 1, 6);
+INSERT INTO pledges VALUES (NULL, 253100, 8, 2);
+INSERT INTO pledges VALUES (NULL, 449700, 1, 1);
+INSERT INTO pledges VALUES (NULL, 186500, 4, 5);
+INSERT INTO pledges VALUES (NULL, 105800, 3, 9);
+INSERT INTO pledges VALUES (NULL, 201500, 4, 7);
+INSERT INTO pledges VALUES (NULL, 205900, 8, 10);
+INSERT INTO pledges VALUES (NULL, 490400, 5, 1);
+INSERT INTO pledges VALUES (NULL, 332700, 14, 10);
+
+
+
+-- CATEGORY --
+
+CREATE TABLE categories (
+  id INTEGER PRIMARY KEY,
+  label text  
+);
+
+INSERT INTO categories VALUES (NULL, 'Music');
+INSERT INTO categories VALUES (NULL, 'Books');
+INSERT INTO categories VALUES (NULL, 'Charity');
+
+
+-- PROJECT_CATEGORIES --
+
+CREATE TABLE project_categories (
+  id INTEGER PRIMARY KEY,
+  project_id INTEGER,
+  category_id INTEGER);
+
+INSERT INTO project_categories VALUES (NULL, 1, 3);
+INSERT INTO project_categories VALUES (NULL, 2, 2);
+INSERT INTO project_categories VALUES (NULL, 3, 2);
+INSERT INTO project_categories VALUES (NULL, 4, 1);
+INSERT INTO project_categories VALUES (NULL, 5, 1);
+INSERT INTO project_categories VALUES (NULL, 6, 3);
+INSERT INTO project_categories VALUES (NULL, 7, 1);
+INSERT INTO project_categories VALUES (NULL, 8, 1);
+INSERT INTO project_categories VALUES (NULL, 9, 3);
+INSERT INTO project_categories VALUES (NULL, 10, 3);
+
+
+
+
 -- 1. Select the titles of all projects and their pledge amounts.
 
 SELECT title, SUM(amount)/100 
@@ -135,134 +268,4 @@ GROUP BY projects.id;
 -- Books|855
 
 
-
----------------------
--- BUILD DB SOURCE --
----------------------
-
-
--- PROJECTS --
-
-CREATE TABLE projects (
-  id INTEGER PRIMARY KEY,
-  title text,
-  funding_goal INTEGER,
-  start_date text,
-  end_date text
-);
-
-INSERT INTO projects VALUES (NULL, 'iPhone Solar Charger Case', 190000, '2013-10-01', '2013-11-01');
-INSERT INTO projects VALUES (NULL, 'iPhone Wireless Charger', 180000, '2013-10-02', '2013-11-03');
-INSERT INTO projects VALUES (NULL, 'iPhone External Monitor', 170000, '2013-10-03', '2013-11-05');
-INSERT INTO projects VALUES (NULL, 'iPhone USB Taste Sensor', 160000, '2013-10-04', '2013-11-07');
-INSERT INTO projects VALUES (NULL, 'iPhone Invisibility Case', 150000, '2013-10-05', '2013-11-15');
-INSERT INTO projects VALUES (NULL, 'iPhone Screen Projector', 140000, '2013-10-10', '2013-11-11');
-INSERT INTO projects VALUES (NULL, 'iPhone Wool Knit Case', 130000, '2013-10-15', '2013-11-01');
-INSERT INTO projects VALUES (NULL, 'iPhone External SMS Notification LED', 120000, '2013-10-20', '2013-10-30');
-INSERT INTO projects VALUES (NULL, 'iPhone Foldable Keyboard', 110000, '2013-10-30', '2013-11-31');
-INSERT INTO projects VALUES (NULL, 'iPhone Thunderbolt-to-classic Charger Converter', 810000, '2013-12-01', '2013-12-31');
-
-
--- USERS --
-
-CREATE TABLE users (
-  id INTEGER PRIMARY KEY,
-  name text,
-  age INTEGER
-);
-
-INSERT INTO users VALUES (NULL, 'Lavona Parrales', 42);
-INSERT INTO users VALUES (NULL, 'Iliana Yamanaka', 23);
-INSERT INTO users VALUES (NULL, 'Daniela Vida', 54);
-INSERT INTO users VALUES (NULL, 'Cletus Pipes', 62);
-INSERT INTO users VALUES (NULL, 'Ashton Roda', 21);
-INSERT INTO users VALUES (NULL, 'Alexander Handley', 23);
-INSERT INTO users VALUES (NULL, 'Christie Touchet', 54);
-INSERT INTO users VALUES (NULL, 'Sharen Mccubbin', 35);
-INSERT INTO users VALUES (NULL, 'Valerie Heiss', 42);
-INSERT INTO users VALUES (NULL, 'Chasidy Swink', 62);
-INSERT INTO users VALUES (NULL, 'Lovie Byars', 37);
-INSERT INTO users VALUES (NULL, 'Ernestine Ring', 80);
-INSERT INTO users VALUES (NULL, 'Kiley Mcferrin', 15);
-INSERT INTO users VALUES (NULL, 'Hsiu Summerville', 36);
-INSERT INTO users VALUES (NULL, 'Iris Bogle', 42);
-INSERT INTO users VALUES (NULL, 'Meagan Waechter', 44);
-INSERT INTO users VALUES (NULL, 'Karry Sheahan', 32);
-INSERT INTO users VALUES (NULL, 'Michale Minard', 18);
-INSERT INTO users VALUES (NULL, 'Andria Sollars', 26);
-INSERT INTO users VALUES (NULL, 'Kasie Sudler', 28);
-
-
-
--- PLEDGES --
-
-CREATE TABLE pledges (
-  id INTEGER PRIMARY KEY,
-  amount INTEGER,
-  user_id INTEGER,
-  project_id INTEGER
-);
-
-INSERT INTO pledges VALUES (NULL, 344000, 7, 4);
-INSERT INTO pledges VALUES (NULL, 352100, 12, 10);
-INSERT INTO pledges VALUES (NULL, 266200, 1, 5);
-INSERT INTO pledges VALUES (NULL, 113900, 2, 10);
-INSERT INTO pledges VALUES (NULL, 165800, 12, 9);
-INSERT INTO pledges VALUES (NULL, 191300, 15, 5);
-INSERT INTO pledges VALUES (NULL, 151200, 11, 2);
-INSERT INTO pledges VALUES (NULL, 491900, 1, 7);
-INSERT INTO pledges VALUES (NULL, 21700, 2, 4);
-INSERT INTO pledges VALUES (NULL, 295500, 17, 5);
-INSERT INTO pledges VALUES (NULL, 67500, 8, 9);
-INSERT INTO pledges VALUES (NULL, 27600, 9, 2);
-INSERT INTO pledges VALUES (NULL, 240600, 19, 4);
-INSERT INTO pledges VALUES (NULL, 391900, 2, 4);
-INSERT INTO pledges VALUES (NULL, 17000, 0, 7);
-INSERT INTO pledges VALUES (NULL, 35600, 2, 8);
-INSERT INTO pledges VALUES (NULL, 406400, 20, 2);
-INSERT INTO pledges VALUES (NULL, 411000, 17, 5);
-INSERT INTO pledges VALUES (NULL, 497900, 19, 4);
-INSERT INTO pledges VALUES (NULL, 432500, 15, 10);
-INSERT INTO pledges VALUES (NULL, 85500, 8, 3);
-INSERT INTO pledges VALUES (NULL, 97200, 1, 6);
-INSERT INTO pledges VALUES (NULL, 253100, 8, 2);
-INSERT INTO pledges VALUES (NULL, 449700, 1, 1);
-INSERT INTO pledges VALUES (NULL, 186500, 4, 5);
-INSERT INTO pledges VALUES (NULL, 105800, 3, 9);
-INSERT INTO pledges VALUES (NULL, 201500, 4, 7);
-INSERT INTO pledges VALUES (NULL, 205900, 8, 10);
-INSERT INTO pledges VALUES (NULL, 490400, 5, 1);
-INSERT INTO pledges VALUES (NULL, 332700, 14, 10);
-
-
-
--- CATEGORY --
-
-CREATE TABLE categories (
-  id INTEGER PRIMARY KEY,
-  label text  
-);
-
-INSERT INTO categories VALUES (NULL, 'Music');
-INSERT INTO categories VALUES (NULL, 'Books');
-INSERT INTO categories VALUES (NULL, 'Charity');
-
-
--- PROJECT_CATEGORIES --
-
-CREATE TABLE project_categories (
-  id INTEGER PRIMARY KEY,
-  project_id INTEGER,
-  category_id INTEGER);
-
-INSERT INTO project_categories VALUES (NULL, 1, 3);
-INSERT INTO project_categories VALUES (NULL, 2, 2);
-INSERT INTO project_categories VALUES (NULL, 3, 2);
-INSERT INTO project_categories VALUES (NULL, 4, 1);
-INSERT INTO project_categories VALUES (NULL, 5, 1);
-INSERT INTO project_categories VALUES (NULL, 6, 3);
-INSERT INTO project_categories VALUES (NULL, 7, 1);
-INSERT INTO project_categories VALUES (NULL, 8, 1);
-INSERT INTO project_categories VALUES (NULL, 9, 3);
-INSERT INTO project_categories VALUES (NULL, 10, 3);
 
